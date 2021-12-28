69c39b9c7b18f3f26dd451743c29f3d11b68bfaa
Abdi <alldayabdi@gmail.com>
2021-12-28 12:08:20 -0600

    Finished stretch goal 7 and 8

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -57,11 +57,31 @@ for (let x = 0; x < supplyChanges.length; x++) {
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop.
+
 console.log('7. Showing supplyChanges with "for of" loop');
+for (let x of supplyChanges) {
+ if (x > 0 ) {
+   console.log('Added x parts', x);
+} else if (x < 0) {
+  console.log('Removed x parts', x);
+
+} else if (x =0 ) {
+  console.log('No change', x);
+
+}
+
+}
 
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
+let totalParts = 0;
+for (let i = 0; i < supplyChanges.length; i++) {
+  totalParts += supplyChanges[i];
+  console.log(totalParts);
+
+}
+
 
 // 9. We have a large stash of parts in our warehouse that we
 //    need to box up and get ready for shipment.
